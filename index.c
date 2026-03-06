d08dd06547f932d1b8353a4bb4db06aba82754b7
usman-faisal <usman.shamsi987@gmail.com>
2026-03-07 02:06:43 +0500

    repl implementation

---
+++ index.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct {
+    char* buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+} InputBuffer;
+
+InputBuffer* create_input_buffer(){
+    InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->input_length = 0;
+    input_buffer->buffer_length = 0;
+
+    return input_buffer;
+}
+
+void read_input(InputBuffer *input_buffer) {
+    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+    if (bytes_read <= 0) {
+        exit(EXIT_SUCCESS);
+    }
+
+    // trailing line \n
+    input_buffer->buffer[bytes_read - 1] = '\0';
+    input_buffer->input_length = bytes_read - 1;
+}
+
+void close_input_buffer(InputBuffer* input_buffer) {
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+
+int main() {
+    InputBuffer *input_buffer = create_input_buffer();
+
+    while (true) {
+        printf("db > ");
+        read_input(input_buffer);
+
+        if (strcmp(input_buffer->buffer, ".exit") == 0) {
+            close_input_buffer(input_buffer);
+            exit(EXIT_SUCCESS);
+        }
+
+        printf("Unrecognized command \n");
+    }
+}
